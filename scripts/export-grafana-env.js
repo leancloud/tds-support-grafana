62782ac81b80cf4a9e826dfaff693145b90f2a78
sdjdd <i@sdjdd.com>
2022-08-04 16:53:13 +0800

    feat: config grafana version by env

--- scripts/export-grafana-env.js
+++ scripts/export-grafana-env.js
@@ -1,3 +1,8 @@
 const { grafana } = require('../package.json');
 
-console.log(`export GF_INSTALL_VERSION=${grafana.version}`);
+if (process.env.GF_INSTALL_VERSION) {
+  console.log(`echo "Install Grafana ${process.env.GF_INSTALL_VERSION}"`);
+} else {
+  console.log(`echo "Install Grafana ${grafana.version}"`);
+  console.log(`export GF_INSTALL_VERSION=${grafana.version}`);
+}
